59b9f1adc2cacc7e5e301bec97ccf9dfcfc7ccc7
nsaini007 <nsaini@gammastack.com>
2023-03-15 18:25:19 +0530

    commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+class SessionsController < ApplicationController
+    before_action :redirect_if_authenticated, only: [:create, :new]
+    before_action :authenticate_user!, only: [:destroy]
+
+    def create
+        @user = User.find_by(email: params[:user][:email].downcase)
+        if @user
+            if @user.unconfirmed?
+                redirect_to new_confirmation_path, alert: "Incorrect email or password."
+            elsif @user.authenticate(params[:user][:password])
+                after_login_path = session[:user_return_to] || root_path
+                login @user
+                remember(@user) if params[:user][:remember_me] == "1"
+                redirect_to after_login_path, notice: "Signed in"
+            else
+                flash.now[:alert] = "Incorrect email or password."
+                render :new, status: :unprocessable_entity
+            end
+            
+        else
+            flash.now[:alert] = "Incorrect email or password"
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        forget(current_user)
+        logout
+        redirect_to root_path, notice: "Signed out"
+    end
+
+    def new
+        
+    end
+end
